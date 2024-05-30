6a139d620d20c49bfb83432443eca83d87006018
Niels van Schagen <schagen2004@outlook.com>
2024-05-30 19:45:38 +0200

    std

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "tree.h"
 #include "parser.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
 
@@ -20,6 +21,27 @@ void lclex_help(char *argv[]) {
     fprintf(stderr, "    -p: show parsed expression\n");
     fprintf(stderr, "    -h: hide result expression\n");
 }
+
+char *std_exprs[] = {
+    "def id = \\x.x",
+    "def add = \\m.\\n.\\f.\\x.m f (n f x)",
+    "def succ = add 1",
+    "def mul = \\m.\\n.\\f.\\x.m (n f) x",
+    "def exp = \\m.\\n.n m",
+    "def square = \\m.exp m 2",
+    "def pred = \\n.\\f.\\x.n (\\g.\\h. h (g f)) (\\u x) id",
+    "def sub = \\m.\\n.(n pred) m",
+    "def div = (\\n.((\\f.(\\x.x x) (\\x.f (x x)))" \
+    "(\\c.\\n.\\m.\\f.\\x.(\\d.(\\n.n (\\x.(\\a.\\b.b)) (\\a.\\b.a))" \
+    "d ((\\f.\\x.x) f x) (f (c d m f x))) ((\\m.\\n.n" \
+    "(\\n.\\f.\\x.n (\\g.\\h.h (g f)) (\\u.x) (\\u.u)) m) n m)))" \
+    "((\\n.\\f.\\x. f (n f x)) n))",
+    "opdef ^ 3 = exp",
+    "opdef * 4 = mul",
+    "opdef / 4 = div",
+    "opdef + 5 = add",
+    "opdef - 5 = sub"
+};
 
 int main(int argc, char *argv[]) {
     lclex_options_t opts = {
@@ -66,6 +88,27 @@ int main(int argc, char *argv[]) {
     lclex_init_operator_levels(opdefs);
 
     lclex_parser_signal_t sig = LCLEX_PARSER_SUCCESS;
+
+    for (size_t i = 0; i < sizeof(std_exprs) / sizeof(*std_exprs); i++) {
+        char *command = lclex_strdup(std_exprs[i]);
+        char *text = command;
+        lclex_node_t *expr;
+
+        sig = lclex_parse_statement(&text, &defs, opdefs, &expr);
+
+        if (sig == LCLEX_PARSER_FAILURE) {
+            fprintf(stderr, "Error: syntax error in standard expression\n");
+
+            sig = LCLEX_PARSER_EXIT;
+            break;
+        }
+
+        if (expr != NULL) {
+            lclex_free_node(expr);
+        }
+
+        free(command);
+    }
 
     while (sig != LCLEX_PARSER_EXIT) {
         printf(">>> ");
